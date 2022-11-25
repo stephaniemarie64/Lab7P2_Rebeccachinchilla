73181f514bb1b8f2a57af1c19143a61e1548d38b
stephaniemarie64 <rebeccam.chinchillad@gmail.com>
2022-11-25 15:49:34 -0600

    grupos commit

---
+++ src/lab7p2_rebeccachinchilla/Grupos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7p2_rebeccachinchilla;
+
+/**
+ *
+ * @author Dell
+ */
+public class Grupos {
+ /*
+    Consiste de una tabla de posiciones, y una lista ordenada de 4 Equipos donde el criterio de  
+    ordenamiento deben ser los puntos obtenidos y en caso de empate, el siguiente criterio de  
+    desempate es la diferencia de goles de cada Equipo. En caso de persistir el empate el siguiente  
+    criterio de desempate son los Goles anotados por cada equipo y sí todavía persiste el empate,  
+    quedara arriba el equipo al que le hayan sacado menos tarjetas amarillas. 
+    */
+    
+    
+  //  Seleccion
+    
+    
+    
+    
+}
